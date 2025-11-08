cf76990652ce6dffaea60107127965e296919705
Niclas Kjall-Ohlsson <niclask@microsoft.com>
2025-11-08 22:52:47 +0100

    Changes

--- src/index.browser.ts
+++ src/index.browser.ts
@@ -1,3 +1,11 @@
+/**
+ * FlowQuery - A declarative query language for data processing pipelines.
+ * 
+ * This is the main entry point for the FlowQuery browser interface.
+ * 
+ * @packageDocumentation
+ */
+
 import {default as FlowQuery} from "./compute/runner";
 
 export default FlowQuery;

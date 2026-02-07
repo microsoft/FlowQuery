164ccf9621b17d55a120d3da22f6944b0815cae3
Niclas Kjall-Ohlsson <niclask@microsoft.com>
2026-02-07 18:27:05 +0100

    Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "/flowquery-vscode/test/"
-  ],
+    testEnvironment: "node",
+    transform: {
+        "^.+.tsx?$": ["ts-jest", {}],
+    },
+    testPathIgnorePatterns: ["/node_modules/", "/flowquery-vscode/test/", "/misc/apps/"],
 };
